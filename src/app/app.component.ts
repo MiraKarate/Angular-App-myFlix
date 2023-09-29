908380117208ccc094fd00559c07375887414932
MiraKarate <mira.herrarte@gmail.com>
2023-09-29 13:16:44 +0200

    added user login form component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 // src/app/app.component.ts
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-//import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
-//import { MovieCardComponent } from './movie-card/movie-card.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -21,19 +20,12 @@ export class AppComponent {
       width: '280px'
     });
   }
-  /** 
-    //Opens the login dialog
-    openLoginDialog(): void {
-      this.dialog.open(UserLoginFormComponent, {
-        width: '280px'
-      });
-    }
-  
-    //Opens the movie card dialog.
-  
-    openMoviesDialog(): void {
-      this.dialog.open(MovieCardComponent, {
-        width: '500px'
-      });
-    }*/
+
+  //Opens the login dialog
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      width: '280px'
+    });
+  }
+
 }
---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, Input } from '@angular/core';
+//Use this import to close the dialog on success
+import { MatDialogRef } from '@angular/material/dialog';
+//This import brings in the API calls created in Task 6.2
+import { FetchApiDataService } from '../fetch-api-data.service';
+//Import is used to display notifications back to the user
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss']
+})
+export class UserLoginFormComponent implements OnInit {
+
+  @Input() userData = { Username: '', Password: '' };
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar,
+    private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  //This function is responsible for sending the form inputs to the backend
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe((result) => {
+      //Logic for a successful user registration goes here
+      this.dialogRef.close(); //This will close the modal on success.
+      console.log(result);
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
+    }, (result) => {
+      console.log(result);
+      this.router.navigate(['movies']);
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
+    });
+  }
+
+}
+
