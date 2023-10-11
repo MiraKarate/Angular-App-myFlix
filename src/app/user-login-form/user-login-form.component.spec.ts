54407c32d74bb211985b818d47bc75028007bb6e
MiraKarate <mira.herrarte@gmail.com>
2023-10-11 20:26:44 +0200

    troubleshooting

--- src/app/user-login-form/user-login-form.component.spec.ts
+++ src/app/user-login-form/user-login-form.component.spec.ts
@@ -2,7 +2,10 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserLoginFormComponent } from './user-login-form.component';
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of f4c0e6dd (Auto-generated commit [ci skip])
 describe('UserLoginFormComponent', () => {
   let component: UserLoginFormComponent;
   let fixture: ComponentFixture<UserLoginFormComponent>;
