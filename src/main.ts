e4f20ccc7c1504cdb1452b90fa863d8539daa106
MiraKarate <mira.herrarte@gmail.com>
2024-04-14 17:45:56 +0200

    Migrate to Standalone Components: Step 3
    This commit completes the migration to standalone components by bootstrapping the application using standalone APIs. The Angular CLI has been used to select the appropriate option for bootstrapping, ensuring the application is fully migrated and operational.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,43 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { MatListModule } from '@angular/material/list';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MovieCardComponent } from './app/movie-card/movie-card.component';
+import { WelcomePageComponent } from './app/welcome-page/welcome-page.component';
+import { provideRouter, Routes } from '@angular/router';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app/app-routing.module';
+import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomePageComponent },
+  { path: 'movies', component: MovieCardComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+];
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+
+
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AppRoutingModule, FormsModule, MatDialogModule, MatInputModule, MatButtonModule, MatCardModule, MatFormFieldModule, MatSnackBarModule, MatIconModule, MatToolbarModule, MatDatepickerModule, MatNativeDateModule, MatListModule),
+        provideHttpClient(withInterceptorsFromDi()),
+        provideAnimations(),
+        provideRouter(appRoutes)
+    ]
+})
   .catch(err => console.error(err));
